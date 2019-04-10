c7ed8d63b855771bcb6031afd3c3e449a4b61dac
shubhamsaraswat20 <shubham.saraswat_ccv17@gla.ac.in>
2019-04-10 10:58:25 +0530

    init commit methods and constructor

--- src/Tree.java
+++ src/Tree.java
@@ -13,4 +13,49 @@ public class Tree {
 
     }
 
+    protected Node root;
+
+    public Tree() {
+        this.root = null;
+    }
+
+    protected Tree(Node root) {
+        this.root = root;
+    }
+
+    public Tree(int data, Tree leftTree, Tree rightTree) {
+        this.root = new Node(data);
+        if (leftTree != null) {
+            this.root.leftChild = leftTree.root;
+        }
+        else {
+            /*return null;*/
+        }
+    }
+
+    public Tree getRightSubTree() {
+        if (root != null && root.rightChild != null) {
+            return new Tree(root.rightChild);
+        }
+        else {
+            return null;
+        }
+    }
+
+    public boolean isLeaf() {
+        return (root.leftChild == null && root.rightChild ==null);
+    }
+
+    /*public boolean isLeaf(Node node) {
+        return
+    }*/
+
+    private boolean isNullTree() {
+        boolean response = false;
+        if (this.root == null) {
+            response = true;
+        }
+        return response;
+    }
+
 }
